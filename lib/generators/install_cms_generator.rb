ea7063d86f647d06ffcf10aba89c07d6fee627fa
Sam McDavid <mcdavidsa@gmail.com>
2015-10-02 12:47:45 -0400

    Add ActiveAdmin JS Dependencies
    Why:
    
    When installing Atomic CMS for initial use, the ActiveAdmin javascript
    file needs to be updated to include the necessary JS dependencies.
    
    This change addresses the need by:
    
    Add the dependencies to active_admin.js.cofee for the user upon running
    `rails generate install_cms`.

--- lib/generators/install_cms_generator.rb
+++ lib/generators/install_cms_generator.rb
@@ -1,9 +1,38 @@
 class InstallCmsGenerator < Rails::Generators::Base
+  desc <<-DESC.strip_heredoc
+    Setup ActiveAdmin related functionality.
+  DESC
+
   def install_active_admin
     gem 'activeadmin', '1.0.0.pre2'
     flags = ''
     flags << '--skip-users' unless Gem.loaded_specs.keys.include?('devise')
 
     generate 'active_admin:install', flags
+    initialize_active_admin_assets
+  end
+
+  private
+
+  def initialize_active_admin_assets
+    initialize_active_admin_javascript
+  end
+
+  def initialize_active_admin_javascript
+    javascript_asset = "app/assets/javascripts/active_admin.js.coffee"
+    entries = [
+      "#= require angular",
+      "#= require angular-sanitize",
+      "#= require atomic_cms"
+    ]
+    append_to_file( asset_file: javascript_asset, entries: entries )
+  end
+
+  def append_to_file(options)
+    open(options[:asset_file], "a") do |asset_file|
+      options[:entries].each do |entry|
+        asset_file.puts entry
+      end
+    end
   end
 end
