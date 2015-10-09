b8e3d767378671f4e3d63bfee4813cbd3a303e75
Sam McDavid <mcdavidsa@gmail.com>
2015-10-09 09:50:48 -0400

    Add ActiveAdmin Style Dependencies
    Why:
    
    When initializing the CMS, there is a certain css structure that needs
    to be followed in order for previews to be properly displayed.
    
    This change addresses the need by:
    
    Including the Atomic CMS styling dependency and `#component_preview`
    selector.

--- lib/generators/install_cms_generator.rb
+++ lib/generators/install_cms_generator.rb
@@ -10,6 +10,7 @@ class InstallCmsGenerator < Rails::Generators::Base
 
     generate 'active_admin:install', flags
     initialize_active_admin_assets
+    initialize_active_admin_scss
   end
 
   private
@@ -19,17 +20,31 @@ class InstallCmsGenerator < Rails::Generators::Base
   end
 
   def initialize_active_admin_javascript
-    javascript_asset = "app/assets/javascripts/active_admin.js.coffee"
+    javascript_asset = 'app/assets/javascripts/active_admin.js.coffee'
     entries = [
-      "#= require angular",
-      "#= require angular-sanitize",
-      "#= require atomic_cms"
+      '#= require angular',
+      '#= require angular-sanitize',
+      '#= require atomic_cms'
     ]
     append_to_file( asset_file: javascript_asset, entries: entries )
   end
 
+  def initialize_active_admin_scss
+    scss_asset = 'app/assets/stylesheets/active_admin.scss'
+    entries = [
+      '@import "atomic_cms"',
+      '',
+      '#component_preview {',
+      '  // When editing a page through Atomic CMS',
+      '  // images with broken links should not be displayed.',
+      '  img[src="image"] { display:none !important; }',
+      '}'
+    ]
+    append_to_file( asset_file: scss_asset, entries: entries )
+  end
+
   def append_to_file(options)
-    open(options[:asset_file], "a") do |asset_file|
+    open(options[:asset_file], 'a') do |asset_file|
       options[:entries].each do |entry|
         asset_file.puts entry
       end
